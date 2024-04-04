19b7c656b16d23a25e8dadbd799eb3f210d4e0db
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2024-04-04 17:00:08 +0200

    Remove base class from init file

--- baybe/kernels/__init__.py
+++ baybe/kernels/__init__.py
@@ -1,6 +1,5 @@
 """Kernels for Gaussian process surrogate models."""
 
-from baybe.kernels.base import Kernel
 from baybe.kernels.kernels import MaternKernel
 
-__all__ = ["Kernel", "MaternKernel"]
+__all__ = ["MaternKernel"]
