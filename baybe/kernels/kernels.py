437f3242d99d962c82f3e1e96366338b1342f34a
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2024-04-04 17:01:03 +0200

    Remove parameter name from docstring

--- baybe/kernels/kernels.py
+++ baybe/kernels/kernels.py
@@ -8,7 +8,7 @@ from baybe.kernels.base import Kernel
 
 @define
 class MaternKernel(Kernel):
-    """A Matern kernel using a smoothness parameter nu."""
+    """A Matern kernel using a smoothness parameter."""
 
     nu: float = field(converter=float, validator=in_([0.5, 1.5, 2.5]), default=2.5)
     """A smoothness parameter.
